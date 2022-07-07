41340aede30ab179ba914936039165722c358ac5
Aleksandr <Aleksandr266@list.ru>
2022-07-07 16:44:36 +0300

    верстка таблицы с рецептами

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import Recipe from '../Recipe/Recipe';
 
 function App() {
-  return <div className="App">Мороженка!</div>;
+  return (
+    <Routes>
+      <Route path='/recipe' element={<Recipe />} />
+    </Routes>
+  );
 }
 
 export default App;
