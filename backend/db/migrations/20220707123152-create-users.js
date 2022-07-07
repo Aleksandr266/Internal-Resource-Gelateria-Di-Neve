7991e3856f1144aa5b7e41ebac1414e0ec3109bc
Irina Tikhonova <102436907+Irenella7973@users.noreply.github.com>
2022-07-07 15:40:09 +0300

    Created model User, UserTypes, Categories

---
+++ backend/db/migrations/20220707123152-create-users.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullname: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      userType_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'UserTypes',
+          key: 'id',
+        },
+      },
+      login: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
