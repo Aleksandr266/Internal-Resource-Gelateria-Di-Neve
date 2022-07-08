88d90feb4b7213c4ff47c37a3c0e31069c29c453
Aleksandr <Aleksandr266@list.ru>
2022-07-08 10:02:41 +0300

    таблица с рецептами

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Recipe from '../Recipe/Recipe';
+import RecipesList from '../RecipesList/RecipesList';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Recipe />} />
       <Route path="/recipes" element={<Recipe />} />
+      <Route path="/recipesList" element={<RecipesList />} />
     </Routes>
   );
 }
