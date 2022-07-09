4bc69729f40c18d9a471f22aa7542a650dc1c15a
Nikita Tikhonov <tihonov.n.01@gmail.com>
2022-07-09 20:46:45 +0300

    route CostPrice completed

--- backend/routes/api/costprice.routes.js
+++ backend/routes/api/costprice.routes.js
@@ -1,5 +1,36 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
 const costpriceRouter = require('express').Router();
-const { Recipe, RecipeIngridient } = require('../../db/models');
+const {
+  Recipe, RecipeIngridient, Ingridient, IngridientPrices,
+} = require('../../db/models');
+
+function IngridientPrise(el) {
+  return { id: el.id, price: el.IngridientPrices[0].price };
+}
+
+function flavorList(els) {
+  return { ingridient_id: els.ingridient_id, weight: els.weight };
+}
+
+function RecipesINgridient(el) {
+  const result = el.RecipeIngridients.map((els) => flavorList(els));
+
+  return { id: el.id, compound: result };
+}
+
+function ResultSEBES(priceIngridient, el) {
+  var result = 0;
+  for (let i = 0; i < el.compound.length; i++) {
+    for (let a = 0; a < priceIngridient.length; a++) {
+      if (priceIngridient[a].id === el.compound[i].ingridient_id) {
+        result += (Number(priceIngridient[a].price) * Number(el.compound[i].weight));
+      }
+    }
+  }
+  var rest = result / 10;
+  return { id: el.id, price: rest.toFixed(2) };
+}
 
 costpriceRouter
   .route('/')
@@ -7,7 +38,13 @@ costpriceRouter
     const recipes = await Recipe.findAll(
       { include: [Recipe.RecipeIngridients] },
     );
-    res.json({ recipes });
+    const ingidients = await Ingridient.findAll(
+      { include: [Ingridient.IngridientPrices] },
+    );
+    const priceIngridient = ingidients.map((el) => IngridientPrise(el));
+    const recepesIng = recipes.map((el) => RecipesINgridient(el));
+    const result = recepesIng.map((el) => ResultSEBES(priceIngridient, el));
+    res.json({ result });
   });
 
 module.exports = costpriceRouter;
